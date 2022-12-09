320ad398471739f2d7394d44cd842e4b8fcf00b1
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-12-09 13:48:05 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Menu from "./components/Menu";
+import Home from "./components/Home";
+import AddFood from "./components/AddFood";
+import Admin from "./components/Admin";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Food from "./components/Food";
+import Login from "./components/Login";
+import React, { useEffect, useState } from "react";
+import CustomerMenu from "./components/CustomerMenu";
+import TableNum from "./components/TableNum";
 function App() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("menus/")
+      .then((res) => res.json())
+      .then((data) => setMenu(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WELCOME TO FOODIES RESTAURANT</h1>
+      <h2>The Home of Kenyan Delicacies</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu menu={menu}/>} />
+          <Route path="/addfood" element={<AddFood menu={menu}/>} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/customermenu" element={<CustomerMenu menu={menu} />} />
+          <Route path="/table" element={<TableNum />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
