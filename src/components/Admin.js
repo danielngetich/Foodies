8c35dd29e6a97a3d00f9569a19eacc8dcdd1bfbe
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-12-09 20:24:48 +0300

    add front-end

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -15,7 +15,6 @@ function Admin() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        'Accept': 'application/json'
       },
       body: JSON.stringify(admin),
     })
